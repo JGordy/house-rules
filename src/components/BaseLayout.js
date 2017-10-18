fd79901fc441764cfa3983ba05e942233698d93a
Joseph Gordy <jgordy2424@gmail.com>
2017-10-18 13:58:38 -0400

    Added onClick to the nav Links to handle both navigation and toggling the navbar open/close

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 // import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import {withRouter} from "react-router-dom";
 
-export default class BaseLayout extends Component {
+class BaseLayout extends Component {
   constructor(props) {
     super(props);
   //bind function for navbar here
@@ -11,16 +12,18 @@ export default class BaseLayout extends Component {
   }
 
   navToggle() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-          x.className += " responsive";
+      var navBar = document.getElementById("myNavBar");
+      if (navBar.className === "topnav") {
+          navBar.className += " responsive";
       } else {
-          x.className = "topnav";
+          navBar.className = "topnav";
       }
   }
 
   handleNaviagation = (endpoint) => {
-    console.log("BaseLayout THIS.PROPS.CHILDREN.PROPS.CHILDREN: ", this.props.children.props.children);
+    // console.log("BaseLayout THIS.PROPS.CHILDREN.PROPS.CHILDREN: ", this.props.);
+    this.props.history.push(endpoint)
+    this.navToggle();
     // this.props.history.push(endpoint);
   }
 
@@ -28,19 +31,20 @@ export default class BaseLayout extends Component {
     return (
       <div className='BaseLayout'>
 
-          <nav className="topnav" id="myTopnav">
+          <nav className="topnav" id="myNavBar">
 
-            <Link className="Logo" to='#' onClick={this.handleNaviagation('/')}>House Rules</Link>
+            <Link className="Logo" to='#' onClick={() => this.handleNaviagation('/')}>House Rules</Link>
 
             <Link
-            className="GamesLink" to='#' onClick={this.handleNaviagation('/games')}><i className="material-icons">casino</i>Games
+            className="GamesLink" to='#' onClick={() => this.handleNaviagation('/games')}><i className="material-icons">casino</i>Games
             </Link>
 
-            <Link className="NewGameLink" to='/newGame'><i className="material-icons">add</i>Add Game</Link>
+            <Link className="NewGameLink" to='#'
+            onClick={() => this.handleNaviagation('/newGame')}><i className="material-icons">add</i>Add Game</Link>
 
-            <Link className="AboutLink" to='#' onClick={this.handleNaviagation('/about')}><i className="material-icons">local_library</i>About</Link>
+            <Link className="AboutLink" to='#' onClick={() => this.handleNaviagation('/about')}><i className="material-icons">local_library</i>About</Link>
 
-            <Link className="LogOutLink" to="#" onClick={this.handleNaviagation('/')}><i className="material-icons">chevron_left</i>Log Out</Link>
+            <Link className="LogOutLink" to="#" onClick={() => this.handleNaviagation('/')}><i className="material-icons">chevron_left</i>Log Out</Link>
 
             <div id='icon' className="icon" onClick={this.navToggle}>&#9776;</div>
           </nav>
@@ -49,3 +53,5 @@ export default class BaseLayout extends Component {
     );
   }
 };
+
+export default withRouter(BaseLayout);
